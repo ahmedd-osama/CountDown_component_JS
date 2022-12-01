2ea496e1603a4b255d9d7bf5783b8054b55d1aab
Ahmed Osama <ahmed.osaama10@gmail.com>
2022-12-01 12:53:18 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Frontend Mentor - Launch countdown timer solution
+
+This is a solution to the [Launch countdown timer challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+A count down UI made with JS, HTML, CSS
+### Built with
+
+- Semantic HTML5 markup   
+- CSS custom properties   
+- CSS hover event
+- Flexbox   
+- JavaScript DOM manibulation   
+
+## Author
+
+- Website - [Ahmed Osama](https://www.https://github.com/ahmedd-osama)
+- Frontend Mentor - [@ahmedd-osama](https://www.frontendmentor.io/profile/ahmedd-osama)
---
+++ Scripts/app.js
@@ -0,0 +1,15 @@
+let countdownDate = new Date("Mar 22, 2023 15:37:25");
+let days = document.querySelector(".counter .days .time span")
+let hours = document.querySelector(".counter .hours .time span")
+let minutes = document.querySelector(".counter .minutes .time span")
+let seconds = document.querySelector(".counter .seconds .time span")
+
+let count = setInterval(()=>{
+    let now = new Date().getTime();
+    let interval = countdownDate - now
+    Math.floor(interval / (1000 * 60 * 60 * 24)) < 10?days.innerHTML = '0'+Math.floor(interval / (1000 * 60 * 60 * 24)): days.innerHTML = Math.floor(interval / (1000 * 60 * 60 * 24));
+    Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) < 10?hours.innerHTML = '0'+Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)): hours.innerHTML = Math.floor((interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60)) < 10?minutes.innerHTML = '0'+ Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60)): minutes.innerHTML = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
+    Math.floor((interval % (1000 * 60)) / 1000) < 10?seconds.innerHTML = '0'+ Math.floor((interval % (1000 * 60)) / 1000): seconds.innerHTML = Math.floor((interval % (1000 * 60)) / 1000);
+    // UI update
+}, 1000)
